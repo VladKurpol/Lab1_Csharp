533406413c2494e7adc351327878e0f3aad2ef1e
VladKurpol <48331088+VladKurpol@users.noreply.github.com>
2019-03-10 20:12:31 +0200

    .

---
+++ Лабораторная 1-7/Лабораторная 1-7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Press first number:\n");
+
+            double first_age = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Press second number:\n");
+
+            double second_age = Convert.ToDouble(Console.ReadLine());
+
+            double result = first_age * second_age;
+
+            Console.WriteLine($"Your result is - {result}");
+            Console.ReadKey();
+        }
+    }
+}
